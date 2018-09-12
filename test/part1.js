206c973b9082400405df5333a9f38c18b5eb6320
weichaozhan <921391554@qq.com>
2018-09-12 23:36:17 +0800

    file

--- test/part1.js
+++ test/part1.js
@@ -1,8 +1,21 @@
 // test1
 let fs = require('fs')
 
-fs.readFile('../package.json', function(er, data) {
-    console.log('er:', er, '\n', 'data:', data)
+// 读取文件
+// fs.readFile('../package.json', function(er, data) {
+//     console.log('er:', er, '\n', 'data:', data)
+// })
+
+/**
+ * 数据流
+ */
+let stream = fs.createReadStream('./testFile.json')
+
+stream.on('data', function (chunk) {
+    console.log('Stream Chunk:', chunk, '\n')
+})
+stream.on('end', function() {
+    console.log('Stream End')
 })
 
 // test2
@@ -17,8 +30,10 @@ let port = 3000
 let server = http.createServer()
 
 server.on('request', function(req, res) {
-    res.writeHead(200, { 'Content-Type': 'text/plain' })
-    res.end('Hello World2 \n')
+    // res.writeHead(200, { 'Content-Type': 'text/plain' })
+    // res.end('Hello World2 \n')
+    res.writeHead('200', {'Content-Type': 'application/json'})
+    fs.createReadStream('./testFile.json').pipe(res) // 读取流到写出流的管道
 })
 server.listen(port)
 console.log(`server running at port ${port}`)
