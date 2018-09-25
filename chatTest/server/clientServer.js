d5471b3799050a214f4bd87093f0cbd7280a13f2
weichaozhan <921391554@qq.com>
2018-09-25 22:21:09 +0800

    chat 处理进入聊天室逻辑

--- chatTest/server/clientServer.js
+++ chatTest/server/clientServer.js
@@ -2,7 +2,9 @@ const socketio = require('socket.io')
 
 let io
 let guestNumber = 1
+// 保存名称
 let nickName = {}
+// 保存已占用昵称
 let nameUsed = []
 let currentRoom = {}
 
@@ -23,6 +25,52 @@ function assignGuestName(socket, guestNumber, nickName, nameUsed) {
 
   // 计数器增加
   return guestNumber + 1
+}
+
+/**
+ * @description 进入聊天室
+ */
+function joinRoom(socket, room) {
+  // 进入房间
+  socket.join(room)
+  // 记录当前房间
+  currentRoom[socket.id] = room
+  // 告知用户所进入房间的信息
+  socket.emit('joinResult', {
+    room: room
+  })
+  // 告知当前房间其他用户有新用户进入房间
+  socket.broadcast.to(room).emit('message', {
+    text: `${nickName[socket.id]} 加入房间 ${room}。`
+  })
+
+  // 当前房间的用户
+  let usersInRoom = io.sockets.clients(room)
+
+  // 如果当前房间的用户不止一个，汇总用户信息
+  if (usersInRoom.length > 1) {
+    let usersRoomSummary = `当前 ${room} 房间的用户有：`
+    
+    for (let index in usersInRoom) {
+      let userSocketId = usersInRoom[index].id
+
+      // id 不为当前用户的 id
+      if (userSocketId !== socket.id) {
+        if (index > 0) {
+          usersRoomSummary += '，'
+        }
+
+        usersRoomSummary += nickName[userSocketId]
+      }
+    }
+
+    usersRoomSummary += '。'
+
+    // 告知当前用户房间内其他用户的信息
+    socket.emit('message', {
+      text: usersRoomSummary
+    })
+  }
 }
 
 exports.listen = function(server) {
