20a367356d0d15d38266a7fde4685f35b41ecb62
詹伟超 <weichao.zhan@kangcenet.com>
2018-11-16 17:19:37 +0800

    node 基础

---
+++ basisStudy/firstModule.js
@@ -0,0 +1,12 @@
+const privateVar = 'privateVar'
+
+const first = 'I am first'
+const second = `I'm second`
+
+// exports.first = 'I am first'
+// exports.second = `I'm second`
+
+module.exports = a = {
+    first: first,
+    second: second
+}
---
+++ basisStudy/simpleChat.js
@@ -0,0 +1,56 @@
+const events = require('events')
+const net = require('net')
+const channel = new events.EventEmitter()
+
+channel.clients = {}
+channel.subscriptions = {}
+
+channel.on('join', function(id, client) {
+    this.clients[id] = client
+    this.subscriptions[id] = function(senderId, message) {
+        if (id !== senderId) {
+            this.clients[id].write(message)
+        }
+    }
+    this.on('broadcast', this.subscriptions[id])
+    console.log('当前连接数：', this.listeners('broadcast').length, '\n')
+})
+
+channel.on('leave', function(id) {
+    channel.removeListener('broadcast', this.subscriptions[id])
+    this.emit('broadcast', id, `${id} is leaved.\n`)
+    
+    // 删除对应事件
+    delete this.subscriptions[id]
+    console.log('当前连接数：', this.listeners('broadcast').length, '\n')
+    // 所有连接断开关闭服务
+    if (Object.keys(this.subscriptions).length === 0) {
+        server.close()
+    }
+})
+
+const server = net.createServer((client) => {
+    const id = `${client.remoteAddress}:${client.remotePort}`
+
+    client.on('data', (data) => {
+        const newData = data.toString()
+        channel.emit('broadcast', id, newData)
+    })
+    // socket 关闭触发
+    client.on('close', () => {
+        console.log(id)
+        channel.emit('leave', id)
+    })
+})
+
+// 新连接建立成功
+server.on('connection', (client) => {
+    const id = `${client.remoteAddress}:${client.remotePort}`
+
+    channel.emit('join', id, client)
+})
+// server 关闭 --> 所有连接断开
+server.on('close', () => {
+    console.log('Server Closed')
+})
+server.listen(8888)
