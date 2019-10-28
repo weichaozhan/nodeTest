78d1aec5938ce50de2caffeb3be56e950116dc84
wczhan <921391554@qq.com>
2019-10-28 17:59:33 +0800

    lint

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,30 +1,30 @@
 module.exports = {
-    'env': {
-        'browser': true,
-        'commonjs': true,
-        'es6': true
+  'env': {
+    'browser': true,
+    'commonjs': true,
+    'es6': true,
+    'node': true
+  },
+  'extends': [
+    'eslint:recommended', 'alloy', 'alloy/typescript'
+  ],
+  'globals': {
+    'Atomics': 'readonly',
+    'SharedArrayBuffer': 'readonly'
+  },
+  'parser': '@typescript-eslint/parser',
+  'parserOptions': {
+    'ecmaVersion': 2018,
+    'sourceType': 'module',
+    'ecmaFeatures': {
+      'modules': true
     },
-    'extends': [
-        'eslint:recommended',
-        'plugin:@typescript-eslint/eslint-recommended',
-        'eslint-config-alloy/typescript'
-    ],
-    'globals': {
-        'Atomics': 'readonly',
-        'SharedArrayBuffer': 'readonly'
-    },
-    'parser': '@typescript-eslint/parser',
-    'parserOptions': {
-        'ecmaVersion': 2018,
-        "sourceType": "module",
-        "ecmaFeatures": {
-            "modules": true
-        },
-        "project": "./tsconfig.json"
-    },
-    'plugins': [
-        '@typescript-eslint'
-    ],
-    'rules': {
-    }
+    'project': './tsconfig.json'
+  },
+  'plugins': ['@typescript-eslint'],
+  'rules': {
+    'semi': ['error', 'always'], 
+    'semi-style': ['error', 'last'],
+    'quotes': ['error', 'single'],
+  }
 };
