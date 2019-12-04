9eac171ba49052fde11a3ca61e5a2a6f1ed6b2f6
wczhan <921391554@qq.com>
2019-12-04 16:57:54 +0800

    add

--- app/test.ts
+++ app/test.ts
@@ -1,3 +1,10 @@
+const obj: Test.ITest ={
+  a: '123',
+  b: 123,
+};
+
+console.log(`\n\n ${JSON.stringify(obj)}`);
+
 export default {
   a: '111',
 };
---
+++ types/test.d.ts
@@ -0,0 +1,6 @@
+declare namespace Test {
+  interface ITest {
+    a: string;
+    b: number;
+  }
+}
