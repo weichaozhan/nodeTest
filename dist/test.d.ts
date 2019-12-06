1566e127e476343bbb2ea6c76ca90135d679f243
wczhan <921391554@qq.com>
2019-12-06 10:57:32 +0800

    dist

--- dist/test.d.ts
+++
@@ -1,4 +0,0 @@
-declare const _default: {
-    a: string;
-};
-export default _default;
--- dist/test.js
+++
@@ -1,6 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-exports.default = {
-    a: '111',
-};
-//# sourceMappingURL=test.js.map
