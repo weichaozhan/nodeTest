4e74a8981856858666e2e564ead571fd1355ffa5
wczhan <921391554@qq.com>
2019-12-10 18:54:02 +0800

    static source server

--- app/index.ts
+++ app/index.ts
@@ -1,5 +1,23 @@
+import Koa from 'koa';
+import koaStatic from 'koa-static';
+
+import path from 'path';
+
+import tools from './tools';
+
+const app = new Koa();
+const port = 8089;
+const staticPath = './html';
+
+app.use(koaStatic(path.join(__dirname, staticPath)));
+app.listen(port, () => {
+  tools.log('green', `\n\n\nServer listening at port ${port}\n\n\n`);
+});
+
 // import './nodeBaseTest/test';
 // import './nodeBaseTest/fileOption';
 // import './nodeBaseTest/network';
 // import './nodeBaseTest/process';
-import './nodeBaseTest/crypto';
+// import './nodeBaseTest/crypto';
+
+
