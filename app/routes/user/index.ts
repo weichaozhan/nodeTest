9b324895d20a2b3d4a6c7a1507f9dcdee4b5f8a9
wczhan <921391554@qq.com>
2019-12-11 17:38:00 +0800

    获取users

--- app/routes/user/index.ts
+++ app/routes/user/index.ts
@@ -2,15 +2,12 @@ import Router from 'koa-router';
 
 import {
   saveUser,
+  getUsersList,
 } from '../../components/user';
 
 const user = new Router();
 
-user.get('/list', (ctx, next) => {
-  ctx.body = [1, 2, 3, 4, 5, 6];
-  
-  next();
-});
+user.get('/', getUsersList);
 user.post('/', saveUser);
 
 export default user;
