437394e4f539a30693b2b94df7f182c74dd5d7fe
wczhan <921391554@qq.com>
2019-12-24 11:24:11 +0800

    linked list

---
+++ app/tools/dataStructure.ts
@@ -0,0 +1,35 @@
+export class LinkNode {
+  public element: any;
+  public next: any;
+
+  public constructor(element) {
+    this.element = element;
+    this.next = null;
+  }
+}
+
+export class LinkedList {
+  public head: LinkNode;
+  public length: number;
+
+  public constructor() {
+    this.head = null;
+    this.length = 0;
+  }
+
+  public appendEle() {
+
+  }
+
+  public insert(position: number) {
+    
+  }
+
+  public remove(position: number) {
+    
+  }
+
+  public replace(position: number, newEle: any) {
+    
+  }
+}
