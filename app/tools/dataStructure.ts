fe2a5d000bbc52300aaab8cfef18260e13c44baa
wczhan <921391554@qq.com>
2019-12-26 17:56:49 +0800

    link list

--- app/tools/dataStructure.ts
+++ app/tools/dataStructure.ts
@@ -1,9 +1,9 @@
 export class LinkNode {
-  public element: any;
+  public data: any;
   public next: any;
 
-  public constructor(element) {
-    this.element = element;
+  public constructor(data) {
+    this.data = data;
     this.next = null;
   }
 }
@@ -17,19 +17,104 @@ export class LinkedList {
     this.length = 0;
   }
 
-  public appendEle() {
+  public append(nodeData: any) {
+    const newNode = new LinkNode(nodeData);
+    let current: LinkNode = undefined;
+    
+    if (this.head === null) {
+      this.head = newNode;
+    } else {
+      current = this.head;
+      
+      while(current.next) {
+        current = current.next;
+      }
+      current.next = newNode;
+    }
 
+    this.length ++;
   }
 
-  public insert(position: number) {
-    
+  public insert(nodeData: any, position: number) {
+    const newNode = new LinkNode(nodeData);
+    let positionHandle = position;
+    let current: LinkNode = undefined;
+    let previous: LinkNode = undefined;
+    let index = 0;
+
+    if (position > this.length) {
+      positionHandle = this.length;
+    } else if (position < 0) {
+      const po = this.length + position
+      positionHandle = po < 0 ? 0 : this.length + position;
+    }
+
+    if (positionHandle === 0) {
+      newNode.next = this.head;
+      this.head = newNode;
+    } else {
+      current = this.head;
+      
+      while(index++ < positionHandle) {
+        previous = current;
+        current = current.next;
+      }
+      previous.next = newNode;
+      newNode.next = current;
+    }
+
+    this.length ++;
   }
 
   public remove(position: number) {
-    
+    let positionHandle = position;
+    let current: LinkNode = undefined;
+    let previous: LinkNode = undefined;
+    let index = 0;
+
+    if (position > this.length) {
+      positionHandle = this.length;
+    } else if (position < 0) {
+      const po = this.length + position
+      positionHandle = po < 0 ? 0 : this.length + position;
+    }
+
+    if (positionHandle === 0) {
+      this.head = this.head.next;
+    } else {
+      current = this.head;
+      
+      while(index++ < positionHandle) {
+        previous = current;
+        current = current.next;
+      }
+      previous.next = (current || {}).next;
+    }
+
+    this.length --;
   }
 
-  public replace(position: number, newEle: any) {
-    
+  public findByIndex(position: number) {
+    let positionHandle = position;
+    let current: LinkNode = undefined;
+    let index = 0;
+
+    if (position > this.length) {
+      positionHandle = this.length;
+    } else if (position < 0) {
+      const po = this.length + position
+      positionHandle = po < 0 ? 0 : this.length + position;
+    }
+
+    if (positionHandle === 0) {
+      return this.head;
+    } else {
+      current = this.head;
+      
+      while(index++ < positionHandle) {
+        current = current.next;
+      }
+      return current
+    }
   }
 }
