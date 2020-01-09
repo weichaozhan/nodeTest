031042fc3f2cfbe1f3834ba66124127a43c3c152
wczhan <921391554@qq.com>
2020-01-09 17:38:07 +0800

    publisher

--- app/tools/index.ts
+++ app/tools/index.ts
@@ -1,3 +1,5 @@
+import { event, } from './eventCustiom';
+
 export const validRequired = (params: any, required: string[]): boolean | string => {
   const lacks = [];
 
@@ -7,3 +9,34 @@ export const validRequired = (params: any, required: string[]): boolean | string
 
   return lacks.length === 0 ? false : `keys ${lacks.join()} is required !`;
 };
+
+const a = () => {
+  console.log('click a');
+};
+const b = () => {
+  console.log('click b');
+};
+
+event.on('click', a);
+
+event.on('click', b);
+
+event.on('click', () => {
+  console.log('click 3');
+});
+
+event.emit('click');
+
+console.log('\n\n\n\n');
+
+event.off('click', a);
+
+event.emit('click');
+
+console.log('\n\n\n\n');
+
+event.off('click');
+
+setTimeout(() => {
+  event.emit('click');
+}, 1000);
