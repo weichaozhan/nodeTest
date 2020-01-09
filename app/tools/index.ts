ec1d14cfc0ef747d66c98781b74952f7894d7cd1
wczhan <921391554@qq.com>
2020-01-09 17:52:44 +0800

    tools

--- app/tools/index.ts
+++ app/tools/index.ts
@@ -1,4 +1,6 @@
-import { event, } from './eventCustiom';
+export * as algorithm from './algorithm';
+export * as dataStructure from './dataStructure';
+export * as eventCustiom from './eventCustiom';
 
 export const validRequired = (params: any, required: string[]): boolean | string => {
   const lacks = [];
@@ -9,34 +11,3 @@ export const validRequired = (params: any, required: string[]): boolean | string
 
   return lacks.length === 0 ? false : `keys ${lacks.join()} is required !`;
 };
-
-const a = () => {
-  console.log('click a');
-};
-const b = () => {
-  console.log('click b');
-};
-
-event.on('click', a);
-
-event.on('click', b);
-
-event.on('click', () => {
-  console.log('click 3');
-});
-
-event.emit('click');
-
-console.log('\n\n\n\n');
-
-event.off('click', a);
-
-event.emit('click');
-
-console.log('\n\n\n\n');
-
-event.off('click');
-
-setTimeout(() => {
-  event.emit('click');
-}, 1000);
