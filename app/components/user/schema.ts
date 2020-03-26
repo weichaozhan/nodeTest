5325f934e57ba0fa4ae9f44559c9d2c5f94dcbad
wczhan <921391554@qq.com>
2020-03-26 11:12:41 +0800

    html

--- app/components/user/schema.ts
+++ app/components/user/schema.ts
@@ -9,4 +9,8 @@ const userSchema = new Schema({
   email: String,
 });
 
+userSchema.methods.speak = () => {
+  console.log('new user', this);
+};
+
 export default mongoose.model('user', userSchema);
