e5e67e56526277122647bcdf6b702c6188a84c2c
wczhan <921391554@qq.com>
2020-04-01 15:33:48 +0800

    lint

--- app/constant/index.ts
+++ app/constant/index.ts
@@ -5,7 +5,7 @@ export const STATUS_CODE = {
   loginTimeout: 401,
 };
 
-export function InitReaponse(code: number = STATUS_CODE.serverErr, msg: string = '服务器错误！', data?: any) {
+export function InitReaponse(code: number = STATUS_CODE.serverErr, msg = '服务器错误！', data?: any) {
   this.code = code;
   this.msg = msg;
   data && (this.data = data);
