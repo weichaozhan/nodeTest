77786d5a610756d5fb65f52f407279e3fdd3b17b
wczhan <921391554@qq.com>
2020-04-02 17:42:40 +0800

    api test

---
+++ app/app.ts
@@ -0,0 +1,19 @@
+import Koa from 'koa';
+import koaStatic from 'koa-static';
+import bodyParser from 'koa-bodyparser';
+import cors from 'koa2-cors';
+
+import { staticPath, } from './constant/gloabal';
+import router from './routes';
+import './mongo';
+
+const app = new Koa();
+
+app.use(cors({
+  origin: 'http://localhost:3000',
+}));
+app.use(bodyParser());
+app.use(koaStatic(staticPath));
+app.use(router.routes()).use(router.allowedMethods());
+
+export default app;
--- app/index.ts
+++ app/index.ts
@@ -1,23 +1,12 @@
-import Koa from 'koa';
-import koaStatic from 'koa-static';
-import bodyParser from 'koa-bodyparser';
 import colors from 'colors';
-import cors from 'koa2-cors';
 
-import { staticPath, } from './constant/gloabal';
+import app from './app';
 import config from './config';
-import router from './routes';
-import './mongo';
 
-const app = new Koa();
 const port = config.port;
 
-app.use(cors({
-  origin: 'http://localhost:3000',
-}));
-app.use(bodyParser());
-app.use(koaStatic(staticPath));
-app.use(router.routes()).use(router.allowedMethods());
 app.listen(port, () => {
   console.log(colors.bold(`\n\n\nServer listening at port ${port}\n\n\n`).green);
 });
+
+export default app;
