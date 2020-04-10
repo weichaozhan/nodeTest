02ca018019e115bd96c1e5f8d2a44bf762037a61
wczhan <921391554@qq.com>
2020-04-10 18:17:50 +0800

    draft

--- html/src/pages/menuManage/Index.tsx
+++ html/src/pages/menuManage/Index.tsx
@@ -1,9 +1,34 @@
-import React from 'react';
+import React, { useState, } from 'react';
+// import ReactDom from 'react-dom';
 import { withRouter, } from 'react-router-dom';
+import { Editor, EditorState, } from 'draft-js';
+import 'draft-js/dist/Draft.css';
 
 const MenuManage = () => {
+  const [editorState, setEditorState] = useState(EditorState.createEmpty());
+
+  const onChange = (editorState: any) => setEditorState(editorState);
+
+  console.log('editorState', editorState.toJS());
   return <div>
-    menu
+    <Editor
+      editorState={editorState}
+      onChange={onChange} 
+      // blockRenderMap={(block: any) => {
+      //   const type = block.getType();
+
+      //   if (type === 'atomic') {
+      //     return {
+      //       component: <div>new div</div>,
+      //       editable: false,
+      //       props: {
+      //         foo: 'bar',
+      //       },
+      //     };
+      //   }
+      //   return null;
+      // }}
+    />
   </div>;
 }
 
