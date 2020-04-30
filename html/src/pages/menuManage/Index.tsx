088bd69d3e79d7174098dadbffb9f4a2b6e8e59d
weichaozhan <921391554@qq.com>
2020-04-30 17:42:27 +0800

    test g6

--- html/src/pages/menuManage/Index.tsx
+++ html/src/pages/menuManage/Index.tsx
@@ -2,99 +2,118 @@ import React, { useEffect, useRef } from 'react';
 import ReactDOM from 'react-dom';
 import { withRouter, } from 'react-router-dom';
 
-import G6, { TreeGraph } from '@antv/g6';
-import { TreeGraphData } from '@antv/g6/lib/types';
+import { TreeGraph, Graph } from '@antv/g6';
+import { TreeGraphData, GraphData } from '@antv/g6/lib/types';
+// import { G6s as G6 } from './g6tools';
+import G6 from './g6tools';
+
+import styles from './G6.module.scss';
+
 // import DraftCM from './DraftCM';
 
-let graph: TreeGraph;
-const data: TreeGraphData = {
+let graph: TreeGraph | Graph | undefined;
+const data: TreeGraphData | GraphData = {
   id: 'root',
   label: 'root',
+  data: {
+    show: true,
+  },
   children: [
     {
-      id: 'subTree1',
-      label: 'subTree1',
-      children: [
-        {
-          id: 'subTree1Child',
-          label: 'subTree1Child',
-        },
-      ],
+      id: 'child1',
+      label: 'child1',
+      data: {
+        show: true,
+      },
     },
     {
-      id: 'subTree2',
-      label: 'subTree2',
-      children: [
-        {
-          id: 'subTree1Child',
-          label: 'subTree1Child',
-        },
-      ],
-    }
-  ]
+      id: 'child2',
+      label: 'child2',
+      data: {
+        show: true,
+      },
+    },
+  ],
 };
+// const data: GraphData = {
+//   nodes: [
+//     {
+//       id: 'node1',
+//       label: 'node1',
+//     },
+//     {
+//       id: 'node2',
+//       label: 'node2',
+//     },
+//   ],
+//   edges: [
+//     {
+//       source: 'node1',
+//       target: 'node2',
+//     },
+//   ],
+// };
 
 const MenuManage = () => {
   const ref = useRef<HTMLInputElement>(null);
 
   useEffect(() => {
     if (!graph && ref.current) {
-      const dom = ReactDOM.findDOMNode(ref.current) as HTMLElement;
-      const width = 900;
-      const height = 800;
+      setTimeout(() => {
+        const dom = ReactDOM.findDOMNode(ref.current) as HTMLElement;
 
-      graph = new G6.TreeGraph({
-        container: dom, // String | HTMLElement，必须，在 Step 1 中创建的容器 id 或容器本身
-        width,
-        height,
-        linkCenter: true,
-        modes: {
-          default: [
-            'zoom-canvas',
-            'drag-node',
-            'click-select'
-          ],
-        },
-        layout: {
-          type: 'compactBox',
-          direction: 'TB',
-        },
-        defaultNode: {
-          type: 'rect',
-          labelCfg: {
-            style: {
-              fill: '#000000A6',
-              fontSize: 10,
+        const width = dom.clientWidth || 900;
+        const height = (document.querySelector('main')?.clientHeight || 500) - 100;
+  
+        graph = new G6.TreeGraph({
+        // graph = new G6.Graph({
+          container: dom, // String | HTMLElement，必须，在 Step 1 中创建的容器 id 或容器本身
+          width,
+          height,
+          modes: {
+            default: [
+              'zoom-canvas',
+              'drag-node',
+              'drag-canvas',
+              'click-select',
+            ],
+          },
+          layout: {
+            type: 'compactBox',
+            // type: 'dagre',
+            direction: 'TB',
+            nodesep: 30,
+            ranksep: 100,
+          },
+          defaultNode: {
+            type: 'bizQNode',
+            labelCfg: {
+              style: {
+                fill: '#000000A6',
+                fontSize: 10,
+              },
             },
           },
-          style: {
-            stroke: '#72CC4A',
-            width: 150,
+          defaultEdge: {
+            type: 'customPolyline',
           },
-        },
-        // defaultEdge: {
-        //   type: 'polyline',
-        //   style: {
-        //     stroke: '#000000',
-        //   },
-        // },
-        nodeStateStyles: {
-          selected: {
-            stroke: 'red'
-          }
-        },
+        });
+  
+        graph.data(data); // 读取 Step 2 中的数据源到图上
+        graph.render(); // 渲染图
+        graph.fitView();
       });
+    }
 
-      graph.data(data); // 读取 Step 2 中的数据源到图上
-      console.log(graph.getNodes());
-      graph.render(); // 渲染图
-      graph.fitView()
+    return () => {
+      (graph as (TreeGraph | Graph)).destroy();
+      graph = undefined;
     }
   }, []);
 
-  return <div>
+  return <div style={{ overflow: 'visible', }} >
     {/* <DraftCM/> */}
-    <div ref={ref} ></div>
+    <div className={styles['wrapper-g6']} ref={ref} ></div>
   </div>;
 }
 
--- html/src/pages/menuManage/g6Tools.ts
+++
@@ -1,5 +0,0 @@
-export const a = '';
-
-export default {
-  a,
-};
