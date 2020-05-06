6f926920cb61a334ca6e69723d08c5f93ccffc98
weichaozhan <921391554@qq.com>
2020-05-06 14:53:58 +0800

    g6

---
+++ html/src/pages/menuManage/g6Constant.ts
@@ -0,0 +1,22 @@
+export const NODE_TYPE = {
+  bizQNode: 'bizQNode',
+  bizQNodeRect: 'bizQNodeRect',
+  childSelectorBizQ: 'childSelectorBizQ',
+  bizQText: 'bizQText',
+  defaultAnswer: 'defaultAnswer',
+  defaultAnswerRect: 'defaultAnswerRect',
+  defaultAnswerText: 'defaultAnswerText',
+};
+export const LINE_TYPE = {
+  customPolyline: 'customPolyline',
+};
+
+export const WIDTH_RECT_NODE = 100;
+
+export const colorMap = {
+  antBlue: '#1890ff',
+  white: '#ffffff',
+  fontColor: '#666',
+  bizQBG: '#cc0000',
+  daBG: '#c8c8c8',
+};
